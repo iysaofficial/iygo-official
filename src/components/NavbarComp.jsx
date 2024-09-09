2b1e850035d0ac0c00e6e733c771c1325917962f
iysaofficial <youngscientist.iysa@gmail.com>
2024-09-09 11:53:54 +0700

    guidebook 2024

--- src/components/NavbarComp.jsx
+++ src/components/NavbarComp.jsx
@@ -46,7 +46,7 @@ const NavbarComp = () =>{
                     {/* <li className='menu-item'><a href="/#category-main-section">Category</a></li> */}
                     {/* <li className='menu-item'><a href="#">FAQ</a></li> */}
                     <li className='menu-item'><a href="#main-category-section">Category</a></li>
-                    {/* <li className='menu-item'><a href="" target='_blank'>Guide Book</a></li> */}
+                    <li className='menu-item'><a href="https://drive.google.com/file/d/1XrDQQnRKNuQNJiv6MrdMxmMi7ruYAKya/view?usp=sharing" target='_blank'>Guide Book</a></li>
                     <li className='menu-item'><a href="#contact-section">Contact</a></li>
                     {/* <li className='menu-item'><a href="https://drive.google.com/drive/folders/1l_bCazIZmdtOSUjAuBAgXTg7YEBTixPY" target='_blank'>Certificate Supervisor</a></li> */}
 
